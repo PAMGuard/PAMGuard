b01cb4d10020aba4b46ba17c6e16fa02c6350e46
Douglas Gillespie <50671166+douggillespie@users.noreply.github.com>
2022-09-13 11:19:31 +0100

    Fix bug in 3DOffline task
    Problems with making catalogue of offline tasks. not important for now,
    but needs revisiting.

--- src/PamController/PamguardVersionInfo.java
+++ src/PamController/PamguardVersionInfo.java
@@ -31,12 +31,12 @@ public class PamguardVersionInfo {
 	 * Version number, major version.minorversion.sub-release.
 	 * Note: can't go higher than sub-release 'f'
 	 */
-	static public final String version = "2.02.04a";
+	static public final String version = "2.02.04aa";
 	
 	/**
 	 * Release date
 	 */
-	static public final String date = "23 June 2022";
+	static public final String date = "13 September 2022";
 	
 //	/**
 //	 * Release type - Beta or Core
