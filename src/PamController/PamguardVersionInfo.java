e169ccb1a782c5c675228b0b399177254343fa19
Douglas Gillespie <50671166+douggillespie@users.noreply.github.com>
2023-01-10 11:17:44 +0000

    Version 2.02.07
    Final readme, POM and version numbering for release 2.02.07

--- src/PamController/PamguardVersionInfo.java
+++ src/PamController/PamguardVersionInfo.java
@@ -31,12 +31,12 @@ public class PamguardVersionInfo {
 	 * Version number, major version.minorversion.sub-release.
 	 * Note: can't go higher than sub-release 'f'
 	 */
-	static public final String version = "2.02.06c";
+	static public final String version = "2.02.07";
 	
 	/**
 	 * Release date
 	 */
-	static public final String date = "13 December 2022";
+	static public final String date = "10 January 2023";
 	
 //	/**
 //	 * Release type - Beta or Core
