bad2255710a251199bf78e063938f10200c72df9
Douglas Gillespie <50671166+douggillespie@users.noreply.github.com>
2024-07-18 09:47:47 +0100

    Better symbol options
    Add clearer options button to multi option symbol manager panel.

--- src/PamView/symbol/modifier/swing/ModifierTreeNode.java
+++ src/PamView/symbol/modifier/swing/ModifierTreeNode.java
@@ -8,12 +8,13 @@ import java.util.Enumeration;
 import javax.swing.tree.TreeNode;
 
 import PamUtils.PamUtils;
+import PamView.dialog.PamDialogPanel;
 import PamView.symbol.modifier.SymbolModifier;
 
 public class ModifierTreeNode implements TreeNode {
 	private SymbolModifier modifier;
 	private SymbolTreeRoot rootNode;
-	private ArrayList<ChoiceTreeNode> choiceNodes;
+	private ArrayList<TreeNode> choiceNodes;
 
 	public ModifierTreeNode(SymbolTreeRoot rootNode, SymbolModifier modifier) {
 		super();
@@ -22,17 +23,25 @@ public class ModifierTreeNode implements TreeNode {
 		int modBits = modifier.getModifyableBits();
 		int nMod = Integer.bitCount(modBits);
 		choiceNodes = new ArrayList<>();
+
+		int leafIndex = 0;
+
 		for (int i = 0; i < nMod; i++) {
-			choiceNodes.add(new ChoiceTreeNode(this, 1<<PamUtils.getNthChannel(i, modBits), i));
+			choiceNodes.add(new ChoiceTreeNode(this, 1<<PamUtils.getNthChannel(i, modBits), leafIndex++));
 		}
-		
-		
+
+		PamDialogPanel optionsPanel = modifier.getDialogPanel();
+		if (optionsPanel != null) {
+			choiceNodes.add(new OptionsTreeNode(this, modifier, optionsPanel, leafIndex++));
+		}
+
+
 	}
 
 	@Override
 	public TreeNode getChildAt(int childIndex) {
 		return choiceNodes.get(childIndex);
-//		return 
+		//		return 
 	}
 
 	@Override
@@ -73,16 +82,22 @@ public class ModifierTreeNode implements TreeNode {
 	}
 
 	public void setModBitmap(int modBitMap) {
-		for (ChoiceTreeNode cN : choiceNodes) {
-			cN.checkBox.setSelected((cN.selectionBit & modBitMap) != 0);
+		for (TreeNode tN : choiceNodes) {
+			if (tN instanceof ChoiceTreeNode) {
+				ChoiceTreeNode cN = (ChoiceTreeNode) tN;
+				cN.checkBox.setSelected((cN.selectionBit & modBitMap) != 0);
+			}
 		}
 	}
-	
+
 	public int getModBitmap() {
 		int mp = 0;
-		for (ChoiceTreeNode cN : choiceNodes) {
-			if (cN.checkBox.isSelected()) {
-				mp |= cN.selectionBit;
+		for (TreeNode tN : choiceNodes) {
+			if (tN instanceof ChoiceTreeNode) {
+				ChoiceTreeNode cN = (ChoiceTreeNode) tN;
+				if (cN.checkBox.isSelected()) {
+					mp |= cN.selectionBit;
+				}
 			}
 		}
 		return mp;
