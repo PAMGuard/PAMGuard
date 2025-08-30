a432b2405f9c4c0723f0058e421a921ed42a613e
Samuel Tabbutt <st@smruconsulting.com>
2025-08-29 19:01:38 -0700

    don't allow pam work dialog

--- src/PamController/fileprocessing/ReprocessManager.java
+++ src/PamController/fileprocessing/ReprocessManager.java
@@ -42,10 +42,11 @@ public class ReprocessManager {
 	public void startCheckingThread(Frame mainFrame, ReprocessManagerMonitor mon) {
 		CheckWorker checkWorker = new CheckWorker(mainFrame, mon);
 		checkWorker.execute();	
-		synchronized (synch) {
-			workDialog = new PamWorkDialog(mainFrame, 1, "Checking input files and existing output data");
-		}
 		if(PamGUIManager.getGUIType()!=PamGUIManager.NOGUI) {
+			synchronized (synch) {
+				workDialog = new PamWorkDialog(mainFrame, 1, "Checking input files and existing output data");
+			}	
+			
 			workDialog.setVisible(true);
 		}
 		
