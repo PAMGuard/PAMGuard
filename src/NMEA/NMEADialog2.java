373f54ddc6ba99316828e1803a391b06e3db1711
Douglas Gillespie <50671166+douggillespie@users.noreply.github.com>
2026-07-29 14:54:29 +0100

    update nmea help

--- src/NMEA/NMEADialog2.java
+++ src/NMEA/NMEADialog2.java
@@ -56,6 +56,8 @@ public class NMEADialog2 extends PamDialog {
 				newSourceType();
 			}
 		});
+
+		setHelpPoint("mapping.NMEA.docs.configuringNMEADataSource");
 		
 		setDialogComponent(mainPanel);
 	}
