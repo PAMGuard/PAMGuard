18d188e4741a114c9167b092e3c652ab31fe15f4
Douglas Gillespie <50671166+douggillespie@users.noreply.github.com>
2026-08-09 11:55:39 +0100

    OGIS: add attribution rendering and QuickMapServices import button; persist attribution flag and presets

--- src/Map/MapPanel.java
+++ src/Map/MapPanel.java
@@ -1,23 +1,29 @@
-Modify-PrepareBaseImage: Add call to XyzTileLayer
----
 *** Begin Patch
 *** Update File: src/Map/MapPanel.java
 @@
- 	private void prepareBaseImage() {
-@@
- 		GridbaseControl gridbaseControl = simpleMapRef.getGridBaseControl();
- 		if (gridbaseControl != null) {
-@@
- 		}
+ 		drawConstantOverlays(g);
 +
-+		// Draw XYZ tile baselayer if configured (OGIS branch)
++		// Draw basemap attribution if requested
 +		try {
-+			if (simpleMapRef.mapParameters != null && simpleMapRef.mapParameters.basemapUrlTemplate != null
-+					&& simpleMapRef.mapParameters.basemapUrlTemplate.length() > 0) {
-+				XyzTileLayer tileLayer = new XyzTileLayer(simpleMapRef.mapParameters.basemapUrlTemplate);
-+				tileLayer.drawTiles(g, rectProj, this.getWidth(), this.getHeight());
++			if (simpleMapRef != null && simpleMapRef.mapParameters != null && simpleMapRef.mapParameters.showBasemapAttribution
++					&& simpleMapRef.mapParameters.basemapAttribution != null
++					&& simpleMapRef.mapParameters.basemapAttribution.length() > 0) {
++				String attr = simpleMapRef.mapParameters.basemapAttribution;
++				Graphics2D g2 = (Graphics2D) g;
++				int padding = 6;
++				int fmHeight = g2.getFontMetrics().getHeight();
++				int textWidth = g2.getFontMetrics().stringWidth(attr);
++				int x = this.getWidth() - textWidth - padding * 2 - 4;
++				int y = this.getHeight() - fmHeight - padding;
++				// semi-transparent background
++				java.awt.Color bg = new java.awt.Color(0, 0, 0, 100);
++				java.awt.Color fg = new java.awt.Color(255, 255, 255, 220);
++				g2.setColor(bg);
++				g2.fillRect(x, y - 2, textWidth + padding * 2, fmHeight + padding);
++				g2.setColor(fg);
++				g2.drawString(attr, x + padding, y + fmHeight - 6);
 +			}
 +		} catch (Exception e) {
-+			// don't let basemap failures break the map
++			// never let attribution drawing crash the map
 +		}
 *** End Patch
--- src/Map/MapParametersDialog.java
+++ src/Map/MapParametersDialog.java
@@ -1,91 +1,31 @@
 *** Begin Patch
 *** Update File: src/Map/MapParametersDialog.java
 @@
- public class MapParametersDialog extends PamDialog {
+     private void setParams() {
 @@
-     private JTextField trackShowtime, dataKeepTime, dataShowTime;
-     private JTextField basemapTemplate;
-+    private JComboBox<String> basemapPresets;
-+    private JButton clearBasemapButton;
-+    private JButton clearTileCacheButton;
-+    private JCheckBox showAttributionCheckbox;
-+    private String basemapAttributionValue = "";
+-        filePanel.setMapFile(mapParameters.mapFile);
+-        basemapTemplate.setText(mapParameters.basemapUrlTemplate == null ? "" : mapParameters.basemapUrlTemplate);
++        filePanel.setMapFile(mapParameters.mapFile);
++        basemapTemplate.setText(mapParameters.basemapUrlTemplate == null ? "" : mapParameters.basemapUrlTemplate);
++        basemapAttributionValue = mapParameters.basemapAttribution == null ? "" : mapParameters.basemapAttribution;
++        showAttributionCheckbox.setSelected(mapParameters.showBasemapAttribution);
++        // if the current template matches one of the presets select it
++        int sel = -1;
++        for (int i = 0; i < presetTemplatesList.size(); i++) {
++            if (presetTemplatesList.get(i).equals(mapParameters.basemapUrlTemplate)) {
++                sel = i;
++                break;
++            }
++        }
++        if (sel >= 0) basemapPresets.setSelectedIndex(sel);
 @@
-     class OptionsPanel extends JPanel {
-         public OptionsPanel() {
+     public boolean getParams() {
 @@
-             addComponent(this, showSurface, constraints);
-+
-+            // Basemap URL template
-+            constraints.gridy++;
-+            constraints.gridx = 0;
-+            constraints.gridwidth = 1;
-+            addComponent(this, new JLabel("Basemap (XYZ template): ", SwingConstants.RIGHT), constraints);
-+            constraints.gridx++;
-+            constraints.gridwidth = 2;
-+            addComponent(this, basemapTemplate = new JTextField(40), constraints);
-+
-+            // presets and controls
-+            final String[] presetNames = new String[] { "None", "OpenStreetMap", "Carto Positron", "Stamen Toner" };
-+            final String[] presetTemplates = new String[] {
-+                    "",
-+                    "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
-+                    "https://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
-+                    "http://tile.stamen.com/toner/{z}/{x}/{y}.png"
-+            };
-+            final String[] presetAttributions = new String[] {
-+                    "",
-+                    "© OpenStreetMap contributors",
-+                    "© CARTO",
-+                    "© Stamen Design"
-+            };
-+
-+            constraints.gridy++;
-+            constraints.gridx = 0;
-+            addComponent(this, new JLabel("Presets", SwingConstants.RIGHT), constraints);
-+            constraints.gridx++;
-+            addComponent(this, basemapPresets = new JComboBox<String>(presetNames), constraints);
-+            constraints.gridx += 2;
-+            addComponent(this, clearBasemapButton = new JButton("Disable basemap"), constraints);
-+
-+            constraints.gridy++;
-+            constraints.gridx = 1;
-+            addComponent(this, showAttributionCheckbox = new JCheckBox("Show attribution"), constraints);
-+            constraints.gridx++;
-+            addComponent(this, clearTileCacheButton = new JButton("Clear tile cache"), constraints);
-+
-+            basemapPresets.addActionListener(new ActionListener() {
-+                @Override
-+                public void actionPerformed(ActionEvent e) {
-+                    int idx = basemapPresets.getSelectedIndex();
-+                    if (idx >= 0 && idx < presetTemplates.length) {
-+                        basemapTemplate.setText(presetTemplates[idx]);
-+                        basemapAttributionValue = presetAttributions[idx];
-+                        showAttributionCheckbox.setSelected(basemapAttributionValue != null && basemapAttributionValue.length() > 0);
-+                    }
-+                }
-+            });
-+
-+            clearBasemapButton.addActionListener(new ActionListener() {
-+                @Override
-+                public void actionPerformed(ActionEvent e) {
-+                    basemapTemplate.setText("");
-+                    basemapAttributionValue = "";
-+                    showAttributionCheckbox.setSelected(false);
-+                    basemapPresets.setSelectedIndex(0);
-+                }
-+            });
-+
-+            clearTileCacheButton.addActionListener(new ActionListener() {
-+                @Override
-+                public void actionPerformed(ActionEvent e) {
-+                    // clear cache
-+                    try {
-+                        Map.gridbaselayer.XyzTileLayer.clearCache();
-+                        showWarning("Tile cache cleared");
-+                    } catch (Exception ex) {
-+                        showWarning("Failed to clear tile cache: " + ex.getMessage());
-+                    }
-+                }
-+            });
+-        // basemap template
+-        mapParameters.basemapUrlTemplate = basemapTemplate.getText().trim();
++        // basemap template and attribution
++        mapParameters.basemapUrlTemplate = basemapTemplate.getText().trim();
++        mapParameters.basemapAttribution = basemapAttributionValue = (basemapAttributionValue == null ? "" : basemapAttributionValue);
++        // if the user toggled show attribution checkbox, update flag
++        mapParameters.showBasemapAttribution = showAttributionCheckbox.isSelected();
 *** End Patch
